3e9a89a6c6c2a9b87563a1f3b5f0309ddc0ece61
André Silva <andreluiz.97@outlook.com>
2023-03-15 19:07:18 -0300

    chore: add tailwindcss

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+}
