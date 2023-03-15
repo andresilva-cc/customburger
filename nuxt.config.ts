56238f27375ee8435348ef772ce35fcf4636c4d4
André Silva <andreluiz.97@outlook.com>
2023-03-15 19:28:50 -0300

    chore: add fonts to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss']
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300&family=Signika:wght@300;400;600&display=swap' }
+      ]
+    }
+  },
+  modules: ['@nuxtjs/tailwindcss'],
 })
