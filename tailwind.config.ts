bc93aeeffbcc9f05487e8153e3633d88f0868aea
André Silva <andreluiz.97@outlook.com>
2023-03-15 19:15:49 -0300

    chore: add colors to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,27 @@
-export default {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-    "./app.vue",
-  ],
+import type { Config } from 'tailwindcss'
+
+export default <Partial<Config>> {
+  theme: {
+    colors: {
+      primary: {
+        500: '#d53734',
+        600: '#c0322f',
+        700: '#a02927'
+      },
+      secondary: {
+        500: '#faa916',
+        600: '#e19814',
+        700: '#c3810a'
+      },
+      light: {
+        50: '#fff7f7',
+        100: '#fdefef',
+        200: '#f5d9d8'
+      },
+      gray: {
+        900: '#26272b'
+      },
+      white: '#ffffff'
+    }
+  }
 }
