5c4ba76a42c61def5f1d160cc0cf426ec907b05b
André Silva <andreluiz.97@outlook.com>
2023-04-06 19:41:42 -0300

    feat: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,18 @@ export default defineNuxtConfig({
     head: {
       title: 'CustomBurger',
       link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#d53734' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300&family=Signika:wght@300;400;600&display=swap' }
+      ],
+      meta: [
+        { name: 'msapplication-TileColor', content: '#ffffff' },
+        { name: 'theme-color', content: '#ffffff' }
       ]
     }
   },
