05a5cd720b3c6ef8a9dc52f3c5dc7b0638ff4910
André Silva <andreluiz.97@outlook.com>
2023-06-28 19:40:08 -0300

    feat: add cursor-pointer to CbCheckbox

--- components/CbCheckbox.vue
+++ components/CbCheckbox.vue
@@ -37,7 +37,7 @@ const baseClasses = computed(() => {
       v-model="isChecked"
       :disabled="props.disabled"
       type="checkbox"
-      class="absolute w-5 h-5 z-[1] opacity-0 peer"
+      class="absolute w-5 h-5 z-[1] opacity-0 peer cursor-pointer"
     >
     <div
       class="flex justify-center items-center absolute w-5 h-5 rounded transition-all ease-out"
