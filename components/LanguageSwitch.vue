89437f7202b5629403e571f81dad67131d03d133
André Silva <andreluiz.97@outlook.com>
2023-06-29 20:12:47 -0300

    feat: change language switch style

--- components/LanguageSwitch.vue
+++ components/LanguageSwitch.vue
@@ -13,8 +13,12 @@ const availableLocales = computed((): LocaleObject[] => {
   <button
     v-for="locale in availableLocales"
     :key="locale.code"
-    class="border-b-4 transition-all"
-    :class="[locale.code === localeProperties.code ? 'border-primary-500' : 'border-transparent']"
+    class="border-b-4 rounded-b-[4px] transition-all hover:transition-none"
+    :class="[
+      locale.code === localeProperties.code ?
+        'border-primary-500 text-primary-500 hover:text-primary-600 active:text-primary-700 hover:border-primary-600 border:text-primary-700'
+        : 'border-transparent hover:opacity-80 active:opacity-60'
+    ]"
     @click="setLocale(locale.code)"
   >
     {{ locale.name }}
