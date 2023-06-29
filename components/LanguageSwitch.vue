6eb21ccfee2871f8aa9fc431c9e8c2f4434d9c02
André Silva <andreluiz.97@outlook.com>
2023-06-29 20:04:28 -0300

    feat: move language switch to its own component

---
+++ components/LanguageSwitch.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { LocaleObject } from '@nuxtjs/i18n/dist/runtime/composables'
+
+const { localeProperties, locales, setLocale } = useI18n()
+
+const availableLocales = computed((): LocaleObject[] => {
+  return locales.value as LocaleObject[]
+})
+</script>
+
+<template>
+  <button
+    v-for="locale in availableLocales"
+    :key="locale.code"
+    class="border-b-4 transition-all"
+    :class="[locale.code === localeProperties.code ? 'border-primary-500' : 'border-transparent']"
+    @click="setLocale(locale.code)"
+  >
+    {{ locale.name }}
+  </button>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,3 @@
-<script setup lang="ts">
-import { computed } from 'vue'
-import { LocaleObject } from '@nuxtjs/i18n/dist/runtime/composables'
-
-const { localeProperties, locales, setLocale } = useI18n()
-
-const availableLocales = computed((): LocaleObject[] => {
-  return locales.value as LocaleObject[]
-})
-</script>
-
 <template>
   <main class="flex justify-center items-center bg-light-50 h-full">
     <div class="px-4 w-full md:w-[300px]">
@@ -27,15 +16,7 @@ const availableLocales = computed((): LocaleObject[] => {
       </div>
     </div>
     <div class="flex gap-4 absolute top-0 right-0 p-4">
-      <button
-        v-for="locale in availableLocales"
-        :key="locale.code"
-        class="border-b-4 transition-all"
-        :class="[locale.code === localeProperties.code ? 'border-primary-500' : 'border-transparent']"
-        @click="setLocale(locale.code)"
-      >
-        {{ locale.name }}
-      </button>
+      <LanguageSwitch />
     </div>
   </main>
 </template>
